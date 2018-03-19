09c4a322e14af3a981a297b8a1c32bffdb61e4a1
lalrishav <lalrishav@gmail.com>
2018-03-19 12:57:48 +0530

    total payment added

--- models/subservice.js
+++ models/subservice.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema
 var subserviceSchema = new Schema({
 	"service"		: {type:mongoose.Schema.Types.ObjectId,ref:'Service'},
 	"subService"	: {"type":String},
-	"charge"		: {"type":String},
+	"serviceCharge"	: {"type":String},
 	"duration"		: {"type":String}
 	/* in number of days */
 })
