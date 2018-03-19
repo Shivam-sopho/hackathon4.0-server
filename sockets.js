434cc0081255f62cf16785f72c74443dbbbb6287
shivam <chotu.kv@gmail.com>
2018-03-19 19:19:28 +0530

    nothing

--- sockets.js
+++ sockets.js
@@ -5,6 +5,8 @@ require("./models/service");
 require("./models/subservice")
 require("./models/college")
 require("./models/details")
+var payment = require("./lib/payment");
+
 var Details = mongoose.model("Details")
 var Subservice = mongoose.model("Subservice")
 var Service = mongoose.model("Service")
@@ -106,6 +108,10 @@ module.exports = function(io){
 
             })
             
+        })
+        socket.on("getPayment",function () {
+            socket.emit(payment);
+            console.log(payment);
         })
     }
 };
