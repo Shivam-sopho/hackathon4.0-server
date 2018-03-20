af8f8ac234924f9e70bc6a4c97ef223f066b9fd9
piyush0901 <piy0901@gmail.com>
2018-03-20 19:51:37 +0530

    feedback frontend

--- routes.js
+++ routes.js
@@ -27,6 +27,10 @@ module.exports = function(app,passport){
         res.render("feedback",{"user":req.user})
     });
 
+    app.get("/checkFeedback",isLoggedIn,function(req,res){
+        res.render("feedbackcheck",{"user":req.user})
+    });
+
     app.get("/logout",function(req,res){
 
     });
@@ -92,6 +96,5 @@ module.exports = function(app,passport){
         failureRedirect : '/',
         failureFlash : true
     }))
-    app.post('/feedbackSubmitted',)
     
 };
